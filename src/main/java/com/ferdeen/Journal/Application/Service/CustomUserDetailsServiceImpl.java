fef2152ef161b33f90693cacb3dc2c57591e4ee4
Ferdeen <ferdeent2002@gmail.com>
2024-12-05 00:41:19 +0530

    userdetailsImpl

---
+++ src/main/java/com/ferdeen/Journal/Application/Service/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ferdeen.Journal.Application.Service;
+
+import com.ferdeen.Journal.Application.Entity.User;
+import com.ferdeen.Journal.Application.Repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user = userRepository.findByUserName(username);
+       if(user != null){
+          UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+                   .username(user.getUserName())
+                   .password(user.getPassword()).roles(user.getRoles().toArray(new String[0]))
+                   .build();
+          return userDetails;
+       }
+        return null;
+    }
+}
